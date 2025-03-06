0b4790d82f82862b870ae3d2de97610d6c482d85
z4raw <ftmhzahra498@gmail.com>
2025-03-06 13:11:46 +0700

    praktikum01 update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pemprograman Web 2
+
+## Repository Praktikum Pemprograman Web 2
+- Nama : Siti Fatimah Azahra
+- NIM : 0110124208
+- Rombel : Si10
---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+// mendefinisikan variabel
+$nama = 'zahra';
+$umur = '18';
+$berat = 60.60;
+
+echo 'Nama saya' . $nama;
+echo '<br/>umur :' . $umur . 'Tahun';
+echo '<br/>Berat' . $nama;
+?>
